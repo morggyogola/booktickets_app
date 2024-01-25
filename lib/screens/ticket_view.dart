80449bb62a432e1e947060f6971290f6b2333227
Morgan Ogola <morganogola@gmail.com>
2024-01-25 15:28:07 +0300

    Overlapping widgets and centering

--- lib/screens/ticket_view.dart
+++ lib/screens/ticket_view.dart
@@ -13,27 +13,67 @@ class TicketView extends StatelessWidget {
     return SizedBox(
       width: size.width,
       height: 200,
-      child:Container(
+      child: Container(
         margin: const EdgeInsets.only(left: 16),
         child: Column(
           children: [
             Container(
               decoration: BoxDecoration(
                 color: Color(0xFF526799),
-                borderRadius:BorderRadius.only(topLeft:Radius.circular(21),topRight:Radius.circular(21)),
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(21),
+                    topRight: Radius.circular(21)),
               ),
               padding: const EdgeInsets.all(16),
               child: Column(
                 children: [
                   Row(
                     children: [
-                      Text("NYC",style: Styles.headLineStyle3.copyWith(color: Colors.white),),
+                      Text(
+                        "NYC",
+                        style:
+                            Styles.headLineStyle3.copyWith(color: Colors.white),
+                      ),
                       Spacer(),
                       ThickContainer(),
+                      Expanded(
+                          child: Stack(
+                            children:[ SizedBox(
+                              height: 24,
+                              child: LayoutBuilder(
+                                builder: (BuildContext context,
+                                    BoxConstraints constraints) {
+                                  return Flex(
+                                    direction: Axis.horizontal,
+                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                    mainAxisSize: MainAxisSize.max,
+                                    children: List.generate(
+                                        (constraints.constrainWidth() / 6).floor(),
+                                            (index) => SizedBox(
+                                          width: 3,
+                                          height: 1,
+                                          child: DecoratedBox(
+                                            decoration: BoxDecoration(
+                                                color: Colors.white),
+                                          ),
+                                        )),
+                                  );
+                                },
+                              ),
+                            ),
+                              Center(child: Transform.rotate(angle: 1.5,child: Icon(Icons.local_airport_rounded,color: Colors.white,))),] ,
+                          )),
+
+                      ThickContainer(),
                       Spacer(),
-                      Text("London",style: Styles.headLineStyle3.copyWith(color: Colors.white),),
+                      Text(
+                        "London",
+                        style:
+                            Styles.headLineStyle3.copyWith(color: Colors.white),
+                      ),
                     ],
-                  )
+                  ),
+
                 ],
               ),
             )
